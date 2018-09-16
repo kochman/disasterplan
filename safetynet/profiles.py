354653031dbe0f2e7721e6823aaba3e25d4fcb77
Justin Gaskins <gaskij@rpi.edu>
2018-09-16 02:36:26 -0400

    Merged the style and master branches

--- safetynet/profiles.py
+++ safetynet/profiles.py
@@ -1,16 +1,46 @@
-from .models import Profile
+from math import sqrt
+from random import getrandbits
+
+from .models import Profile, ProfileToken
+from .database import db
 
 
 def create_profile(data):
-    profile = Profile(
-        name=data["name"],
-        latitude=data["latitude"],
-        longitude=data["longitude"],
-        status=data["status"],
-    )
-    profile.save()
+    with db.atomic():
+        profile = Profile(
+            name=data["name"], latitude=data["latitude"], longitude=data["longitude"]
+        )
+        profile.status = data.get("status", None)
+        profile.phone_number = data.get("phone_number", None)
+        profile.save()
+
+        pt = ProfileToken(profile=profile, token=getrandbits(256))
+        pt.save()
     return profile
+
+
+def update_profile(data, profile_id):
+    with db.atomic():
+        profile = Profile.get_by_id(profile_id)
+        for key, value in data.items():
+            setattr(profile, key, value)
+        profile.save()
+    return profile
+
+
+def get_profile(profile_id):
+    return Profile.get_by_id(profile_id)
 
 
 def get_profiles():
     return (p for p in Profile.select())
+
+
+# take lat and long to return nearby profiles within a certain limit
+def get_nearby_profiles(lat, lon):
+    profiles = []
+    limit = 10
+    for p in Profile.select():
+        if (sqrt((p.latitude - lat) ** 2 + (p.longitude - lon) ** 2)) < limit:
+            profiles.append(p)
+    return profiles
