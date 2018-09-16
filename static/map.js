337c9676f6cdc393ac7352b757923bed41765d0e
Justin Gaskins <gaskij@rpi.edu>
2018-09-16 12:08:40 -0400

    added style to the profile update page

--- static/map.js
+++ static/map.js
@@ -12,17 +12,29 @@ function getMapPoints() {
         marker = new google.maps.Marker({
         position: new google.maps.LatLng(42, -73),
         map: map*/
+       var personList = "<ul>";
         for(i = 0; i < profiles.length; i++){
-            marker = new google.maps.Marker({
-        position: new google.maps.LatLng(profiles[i].latitude, profiles[i].longitude),
-        map: map
-      });
-             google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-            infowindow.setContent(profiles[i].name, profiles[i].phone, profiles[i].status);
-          infowindow.open(map, marker);
+            personList += "<li>" + profiles[i].name + "\n";
+           if (profiles[i].phone_number != null) {
+              personList += profiles[i].phone_number + "\n";
+           } else if (profiles[i].status != null) {
+              personList += profiles[i].status;
+           }
+            personList += "</li>";
+           
+           
+           marker = new google.maps.Marker({
+              position: new google.maps.LatLng(profiles[i].latitude, profiles[i].longitude),
+              map: map
+            });
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+              return function() {
+                  infowindow.setContent(profiles[i].name, profiles[i].phone_number, profiles[i].status);
+                infowindow.open(map, marker);
+              }
+            })(marker, i));
         }
-         })(marker, i));
-        }
+       personList += "</ul>"
+       document.getElementById("nearbyList").innerHTML = personList;
     });
 };
